75245d523f4c0deca76ae73ca9b50bc97cf28195
Lalanke Athauda <lalankeba@gmail.com>
2024-05-23 18:18:04 +0530

    adding test log

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -2,6 +2,7 @@ const logger = require('../logger/logger');
 
 const init = (req, res) => {
     logger.info(`${req.method} ${req.url}`);
+    logger.info('initializing root path..');
     res.status(200).json({ message: 'node API is running' });
 }
 
